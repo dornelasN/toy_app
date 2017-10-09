585a57a773dfe1792a1b350444cf8e5bbc6ded2b
Nelson Dornelas <ndornelasj@gmail.com>
2017-10-09 09:27:59 -0700

    rails scaffold Microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
+  resources :microposts
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  Rails.application.routes.draw do
-  resources :users
-    root 'application#hello'
-  end
+  root 'users#index'
 end
